27c45516f5a68767d09864d068eaa9cf484eec3c
adrea <90507395+adreaisbad@users.noreply.github.com>
2022-07-03 19:38:34 +0700

    Update Content.jsx

--- src/sections/Content.jsx
+++ src/sections/Content.jsx
@@ -9,30 +9,29 @@ function Content() {
         <div className="container mx-auto">
           <div className="text-center mb-20">
             <EyeIcon className="text-green-600 mx-auto inline-block w-10 mb-4" />
+            <iframe
+              width="700"
+              height="700"
+              src="https://www.youtube.com/embed/r4z16al39UI"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+            <br/>
+            <h2 className="title-font sm:text-2xl text-1xl mb-4 text-white">
+              Also My PC Specs
+            </h2>
+            <p className="mb-8 leading-relaxed text-xl text-gray-400">
+              CPU: Intel i7-11700k <br />
+              GPU: Geforce GTX 1650
+              <br />
+              Mouse: Logitech G502 Hero <br />
+              Keyboard: Steelseries Apex Pro <br />
+              Monitor: Gigabyte G34WQC A 34″ 165hz <br />I use Wifi because I'm
+              poor.
+            </p>
           </div>
-
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/r4z16al39UI"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-
-          <h2 className="title-font sm:text-2xl text-1xl mb-4 text-white">
-            Also My PC Specs
-          </h2>
-          <p className="mb-8 leading-relaxed text-xl text-gray-400">
-            CPU: Intel i7-11700k <br />
-            GPU: Geforce GTX 1650
-            <br />
-            Mouse: Logitech G502 Hero <br />
-            Keyboard: Steelseries Apex Pro <br />
-            Monitor: Gigabyte G34WQC A 34″ 165hz <br />I use Wifi because I'm
-            poor.
-          </p>
         </div>
       </div>
     </section>
